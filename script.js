c92839b88a81089d36ea96ac73551835ae0dc078
Barahatsu <barahatsu@gmail.com>
2025-01-14 15:49:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// Función para abrir una imagen en tamaño completo al hacer clic
+document.querySelectorAll('.gallery-item img').forEach(img => {
+    img.addEventListener('click', () => {
+        const src = img.src;
+        const newWindow = window.open();
+        newWindow.document.write(`<img src="${src}" style="width: 100%; height: 100%; object-fit: cover;">`);
+    });
+});
 
